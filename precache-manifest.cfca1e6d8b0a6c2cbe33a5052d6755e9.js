75a083b57241f36cd076c9226e9a9723fdbda57a
Jake Lohman <jakelohman7@gmail.com>
2023-06-23 13:06:06 -0500

    Updates

---
+++ precache-manifest.cfca1e6d8b0a6c2cbe33a5052d6755e9.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fcbd9444ccaef6ea2aa",
+    "url": "/static/css/main.80176129.chunk.css"
+  },
+  {
+    "revision": "6fcbd9444ccaef6ea2aa",
+    "url": "/static/js/main.6fcbd944.chunk.js"
+  },
+  {
+    "revision": "2ff751fcd0360dea16a7",
+    "url": "/static/js/1.2ff751fc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8b5dd88f62dd2f0069bea3c8321609a4",
+    "url": "/static/media/book-beasts-1.8b5dd88f.png"
+  },
+  {
+    "revision": "231e4433e1c85537b8c4ac0274e088c5",
+    "url": "/static/media/book-beasts-2.231e4433.png"
+  },
+  {
+    "revision": "2348025a09251c5371eba70cac1379cc",
+    "url": "/static/media/umami-meats-0.2348025a.png"
+  },
+  {
+    "revision": "d58e87ae9f5dbdfb6192621bed9410ee",
+    "url": "/static/media/umami-meats-1.d58e87ae.png"
+  },
+  {
+    "revision": "8b983589ae27e95d4af7d2e90e07e74c",
+    "url": "/static/media/umami-meats-favicon.8b983589.ico"
+  },
+  {
+    "revision": "df795ab1b45b6c1d6a7a55b8e91460dc",
+    "url": "/static/media/solar-sandbox-1a.df795ab1.jpg"
+  },
+  {
+    "revision": "17392b14a0f87eadfcdfd636a0d77d48",
+    "url": "/static/media/solar-sandbox-1b.17392b14.jpg"
+  },
+  {
+    "revision": "84464e7aa42b4f5d31d5ca5958651b2d",
+    "url": "/static/media/Node.js.84464e7a.png"
+  },
+  {
+    "revision": "de0dbc4293ba7626813b9069007a87c8",
+    "url": "/static/media/PostgreSQL.de0dbc42.png"
+  },
+  {
+    "revision": "d638bd34b425dec39792d4a81189eddf",
+    "url": "/static/media/Express.d638bd34.png"
+  },
+  {
+    "revision": "8802e08798f01cc5454b922221f0bfd0",
+    "url": "/static/media/React.js.8802e087.png"
+  },
+  {
+    "revision": "8251fe76d7487a28b07603515b300964",
+    "url": "/static/media/Redux.8251fe76.png"
+  },
+  {
+    "revision": "9ecb7250e9469dc02c645b795bf56767",
+    "url": "/static/media/Django.9ecb7250.png"
+  },
+  {
+    "revision": "c9421be52fe48fdf04839b53f8e9f1d4",
+    "url": "/static/media/CSS.c9421be5.png"
+  },
+  {
+    "revision": "a38d9c99d73a840ccc95be64e5506fd4",
+    "url": "/static/media/Javascript.a38d9c99.png"
+  },
+  {
+    "revision": "e704390867ca6e26d5be8a7b8865e4a1",
+    "url": "/static/media/headshot.e7043908.jpg"
+  },
+  {
+    "revision": "632c0bb4236238b10049f94084d14fca",
+    "url": "/index.html"
+  }
+];
