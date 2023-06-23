68dc6e53c515889d64a56d2617a16d9f82338089
Jake Lohman <jakelohman7@gmail.com>
2023-06-23 12:30:25 -0500

    Updates

---
+++ precache-manifest.8f52c30ade5fbdffeb71683c013f29b8.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fcbd9444ccaef6ea2aa",
+    "url": "/portfolio/static/css/main.80176129.chunk.css"
+  },
+  {
+    "revision": "6fcbd9444ccaef6ea2aa",
+    "url": "/portfolio/static/js/main.6fcbd944.chunk.js"
+  },
+  {
+    "revision": "c2d88754163ee3199da7",
+    "url": "/portfolio/static/js/1.c2d88754.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "8b5dd88f62dd2f0069bea3c8321609a4",
+    "url": "/portfolio/static/media/book-beasts-1.8b5dd88f.png"
+  },
+  {
+    "revision": "231e4433e1c85537b8c4ac0274e088c5",
+    "url": "/portfolio/static/media/book-beasts-2.231e4433.png"
+  },
+  {
+    "revision": "2348025a09251c5371eba70cac1379cc",
+    "url": "/portfolio/static/media/umami-meats-0.2348025a.png"
+  },
+  {
+    "revision": "d58e87ae9f5dbdfb6192621bed9410ee",
+    "url": "/portfolio/static/media/umami-meats-1.d58e87ae.png"
+  },
+  {
+    "revision": "8b983589ae27e95d4af7d2e90e07e74c",
+    "url": "/portfolio/static/media/umami-meats-favicon.8b983589.ico"
+  },
+  {
+    "revision": "df795ab1b45b6c1d6a7a55b8e91460dc",
+    "url": "/portfolio/static/media/solar-sandbox-1a.df795ab1.jpg"
+  },
+  {
+    "revision": "17392b14a0f87eadfcdfd636a0d77d48",
+    "url": "/portfolio/static/media/solar-sandbox-1b.17392b14.jpg"
+  },
+  {
+    "revision": "84464e7aa42b4f5d31d5ca5958651b2d",
+    "url": "/portfolio/static/media/Node.js.84464e7a.png"
+  },
+  {
+    "revision": "de0dbc4293ba7626813b9069007a87c8",
+    "url": "/portfolio/static/media/PostgreSQL.de0dbc42.png"
+  },
+  {
+    "revision": "d638bd34b425dec39792d4a81189eddf",
+    "url": "/portfolio/static/media/Express.d638bd34.png"
+  },
+  {
+    "revision": "8802e08798f01cc5454b922221f0bfd0",
+    "url": "/portfolio/static/media/React.js.8802e087.png"
+  },
+  {
+    "revision": "8251fe76d7487a28b07603515b300964",
+    "url": "/portfolio/static/media/Redux.8251fe76.png"
+  },
+  {
+    "revision": "9ecb7250e9469dc02c645b795bf56767",
+    "url": "/portfolio/static/media/Django.9ecb7250.png"
+  },
+  {
+    "revision": "c9421be52fe48fdf04839b53f8e9f1d4",
+    "url": "/portfolio/static/media/CSS.c9421be5.png"
+  },
+  {
+    "revision": "a38d9c99d73a840ccc95be64e5506fd4",
+    "url": "/portfolio/static/media/Javascript.a38d9c99.png"
+  },
+  {
+    "revision": "e704390867ca6e26d5be8a7b8865e4a1",
+    "url": "/portfolio/static/media/headshot.e7043908.jpg"
+  },
+  {
+    "revision": "2cb64389f5bca530ec2d751614a010a6",
+    "url": "/portfolio/index.html"
+  }
+];
