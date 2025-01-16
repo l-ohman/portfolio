72de4ec0c6e74f3b836d416ea8d77c137a10e3c4
Jake Lohman <jakelohman7@gmail.com>
2025-01-16 10:10:52 -0600

    Hero tweaks

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,6 @@ import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 import { IoMail, IoDocumentTextSharp } from "react-icons/io5";
 
 const HeroContainer = styled.div`
-  /* box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.125); */
   /* background: ${colors.lighter}; */
   /* padding: 18px; */
 
@@ -17,21 +16,37 @@ const HeroContainer = styled.div`
   h1 {
     font-size: 3rem;
     margin-bottom: 0.95rem;
-  }
-  p {
-    font-size: 1.25rem;
-    margin: 0.85rem 0 0;
+    padding-left: 18px;
   }
 `;
 
+const TextContainer = styled.div`
+  background: ${colors.lighter};
+  padding: 18px;
+  border-radius: 12px;
+  p {
+    font-size: 1.15rem;
+    padding: 0.6rem 0;
+  }
+  z-index: 1;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+`;
+
+const logoP = "12px";
 const LogoLinks = styled.div`
   width: 85%;
-  max-width: 250px;
-  margin: 1rem 0 0;
+  max-width: 275px;
   padding: 0.45em 0 0.1em;
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  background: ${colors.light};
+  padding: calc(12px + ${logoP}) calc(${logoP} * 1.5) ${logoP};
+  margin: -12px 0;
+  z-index: 0;
+  border-radius: 0 0 12px 12px;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.125);
 `;
 const iconSize = 38;
 
@@ -39,8 +54,10 @@ export default function Hero() {
   return (
     <HeroContainer>
       <h1>hi, i'm jake.</h1>
-      <p>I am a fullstack engineer currently based in Austin, TX.</p>
-      <p>Take a look below for my recent projects and contact information.</p>
+      <TextContainer>
+        <p>I am a fullstack engineer currently based in Austin, TX.</p>
+        <p>Take a look below for my recent projects and contact information.</p>
+      </TextContainer>
 
       <LogoLinks>
         <a
